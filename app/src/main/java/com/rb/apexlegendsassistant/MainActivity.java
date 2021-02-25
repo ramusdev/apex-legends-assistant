b78ce4d7e4cb9b0d4e014e9978587c651809b87c
Roman <ram219219@gmail.com>
2021-02-25 13:27:33 +0200

    Menu v1.3

--- app/src/main/java/com/rb/apexlegendsassistant/MainActivity.java
+++ app/src/main/java/com/rb/apexlegendsassistant/MainActivity.java
@@ -2,6 +2,7 @@ package com.rb.apexlegendsassistant;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.Window;
@@ -13,6 +14,7 @@ import java.io.IOException;
 import java.util.List;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -32,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar.setTitle(getResources().getString(R.string.donate_text));
         setSupportActionBar(toolbar);
 
         // Open connection to db
@@ -40,14 +43,32 @@ public class MainActivity extends AppCompatActivity {
         // Tasks after create
         createTasks();
 
+        // mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        // mNavigationView = (NavigationView) findViewById(R.id.shitstuff);
+        DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.bringToFront();
+
+        // navigationView.setNavigationItemSelectedListener(this);
+        // ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name);
+        // drawerLayout.setDrawerListener(mDrawerToggle);
+        // mDrawerToggle.syncState();
         // Navigation
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
+        // DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        // NavigationView navigationView = findViewById(R.id.nav_view);
+
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                drawer.closeDrawers();
+            public boolean onNavigationItemSelected(MenuItem item) {
+                drawerLayout.closeDrawers();
 
+                Log.d("MyTag", String.valueOf(item.getItemId()));
+                Log.d("MyTag", "text");
 
                 if (item.getItemId() == R.id.nav_item_news) {
                     new Handler().postDelayed(new Runnable() {
@@ -56,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
                             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
                             transaction.replace(R.id.nav_host_fragment, NewsFragment.class, null).commit();
                         }
-                    }, 200);
+                    }, 500);
                 }
 
                 if (item.getItemId() == R.id.nav_item_donate) {
@@ -64,9 +85,9 @@ public class MainActivity extends AppCompatActivity {
                         @Override
                         public void run() {
                             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                            transaction.replace(R.id.nav_host_fragment, NewsFragment.class, null).commit();
+                            transaction.replace(R.id.nav_host_fragment, DonateFragment.class, null).commit();
                         }
-                    }, 200);
+                    }, 500);
                 }
 
 
@@ -74,17 +95,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_item_news, R.id.nav_item_donate)
-                .setDrawerLayout(drawer)
-                .build();
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+        // mAppBarConfiguration = new AppBarConfiguration.Builder(
+                // R.id.nav_item_news, R.id.nav_item_donate)
+                // .setDrawerLayout(drawerLayout)
+                // .build();
+
+        // NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+        // NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        // NavigationUI.setupWithNavController(navigationView, navController);
 
 
         // Set window top and bottop color
@@ -98,6 +119,7 @@ public class MainActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().replace(R.id.nav_host_fragment, DonateFragment.class, null).commit();
     }
 
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -111,6 +133,7 @@ public class MainActivity extends AppCompatActivity {
         return NavigationUI.navigateUp(navController, mAppBarConfiguration)
                 || super.onSupportNavigateUp();
     }
+    */
 
     public void updateNewsIfNotExists() {
         NewsLoader newsLoader = new NewsLoader(this.getApplicationContext());
