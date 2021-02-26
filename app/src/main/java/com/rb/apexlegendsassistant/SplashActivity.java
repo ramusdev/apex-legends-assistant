adee929834ee9588ecaf1807769ae28409d99017
Roman <ram219219@gmail.com>
2021-02-26 22:58:25 +0200

    Splash screen v1.1

--- app/src/main/java/com/rb/apexlegendsassistant/SplashActivity.java
+++ app/src/main/java/com/rb/apexlegendsassistant/SplashActivity.java
@@ -1,37 +1,22 @@
 package com.rb.apexlegendsassistant;
 
-import android.annotation.SuppressLint;
-
 import androidx.annotation.RequiresApi;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
-import android.view.MotionEvent;
 import android.view.View;
-import android.view.Window;
-import android.view.WindowManager;
-
-import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN;
-import static android.view.View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
 
 public class SplashActivity extends AppCompatActivity {
 
     private final int SPLASH_TIME = 2000;
 
-    // @RequiresApi(api = Build.VERSION_CODES.P)
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     public void onCreate(Bundle savedInstanceState) {
         overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
         super.onCreate(savedInstanceState);
-
-        // requestWindowFeature(Window.FEATURE_NO_TITLE);//will hide the title
-        // getWindow().getAttributes().layoutInDisplayCutoutMode = WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES;
 
         View decorView = getWindow().getDecorView();
         final int flags = View.SYSTEM_UI_FLAG_LOW_PROFILE
@@ -41,8 +26,6 @@ public class SplashActivity extends AppCompatActivity {
                 | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION;
         decorView.setSystemUiVisibility(flags);
 
-        // getWindow().setFlags(WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_SHORT_EDGES);
-        // toolbar.setVisibility(View.INVISIBLE);
 
         setContentView(R.layout.activity_splash);
 
@@ -55,5 +38,14 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, SPLASH_TIME);
 
+
     }
+
+    /*
+    @Override
+    public void finish() {
+        super.finish();
+        overridePendingTransition(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+    }
+    */
 }
